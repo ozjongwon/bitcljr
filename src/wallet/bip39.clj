51f5f818a2f114febdef7ae53c680802de7d7c61
jc <oz.jongwon.choi@gmail.com>
2024-11-24 23:57:35 +1100

    add checksum

--- src/wallet/bip39.clj
+++ src/wallet/bip39.clj
@@ -102,26 +102,91 @@
       (bit-shift-right n)
       (bit-and num)))
 
-(defn extract-11-bits-dynamic
-  [byte-array len]
-  (let [bytes (map #(bit-and % 0xff) byte-array)
-        bit-length 11]
+(defn sha256 [byte-array]
+  (.digest (java.security.MessageDigest/getInstance "SHA-256") byte-array))
+
+(defn align-byte-array [barray]
+  (let [aligned (loop [nibble (bit-and (second barray) 0x0f) ;; drop first byte and nibble
+                       [b & more-bytes] (nthrest barray 2)
+                       result []]
+                  (if (nil? b)
+                    result
+                    (let [byte (bit-and b 0xff)]
+                      (clojure.pprint/cl-format true "~%>> ~4,'0b + ~8,'0b => ~b"
+                                                nibble
+                                                byte
+                                                (bit-or (bit-shift-left nibble 4)
+                                                        (bit-shift-right byte 4)))
+                      (if (and (= nibble 0) (= byte 0xec))
+                        result
+                        (recur (bit-and byte 0x0f)
+                               more-bytes
+                               (conj result (bit-or (bit-shift-left nibble 4)
+                                                    (bit-shift-right byte 4))))))))]
+    (->> aligned
+         byte-array
+         sha256
+         (map #(bit-and % 0xff))
+         (into aligned))))
+
+(defn extract-11-bits-dynamic [bytearray len]
+  (let [bytes (align-byte-array bytearray)
+        bit-length 11
+        total-bits (* 8 len)]
+    (loop [[byte & more-bytes] bytes
+           carry 0
+           carry-bits 0
+           result []
+           count len]
+      (if (< count 0)
+        (do
+          (clojure.pprint/cl-format true "~%??~8,'0b(~b) " byte carry)
+          result)
+        (let [combined (bit-or (bit-shift-left carry 8) (bit-and byte 0xff)) ; Merge carry and current byte
+              n-available-bits (+ carry-bits 8)] ; Total available bits
+          (clojure.pprint/cl-format true "~%~D: ~8,'0b(~b) => ~b" carry-bits byte carry combined)
+          (if (>= n-available-bits bit-length)
+            ;; If we have enough bits to extract an 11-bit value
+            (let [shift-amount (- n-available-bits bit-length) ; Calculate how many bits to shift to extract the 11 bits
+                  extracted-bits (bit-and (bit-shift-right combined shift-amount) 0x7FF) ; Extract the 11 bit
+                  leftover-value (remove-first-n-bits bit-length combined n-available-bits)] ; Get remaining bits
+              (clojure.pprint/cl-format true " -> ~b" leftover-value)
+              (recur more-bytes
+                     leftover-value
+                     shift-amount
+                     (conj result extracted-bits)
+                     (dec count))) ; Add the 11 bits to the result
+            ;; Not enough bits yet, so we accumulate more from the next byte
+            (recur more-bytes
+                   combined
+                   n-available-bits
+                   result
+                   (dec count)))))))
+  #_
+  (let [bytes-wo-checksum (mapv #(bit-and % 0xff) bytearray)
+        bytes (into bytes-wo-checksum (map #(bit-and % 0xff) (sha256 (byte-array bytes-wo-checksum))))
+        bit-length 11
+        total-bits (* 8 len)
+        checksum-bits (/ total-bits 32)]
+    (clojure.pprint/cl-format true "~%***~a <> ~a " (into [] bytes-wo-checksum) bytes)
     (loop [[byte & more-bytes] (rest bytes)
            carry (first bytes)
            carry-bits 4 ;; first nibble
            result []
            count len]
       (if (zero? count)
-        result
+        (do
+          (clojure.pprint/cl-format true "~%??~8,'0b(~b) " byte carry)
+          result)
         (let [combined (bit-or (bit-shift-left carry 8) (bit-and byte 0xff)) ; Merge carry and current byte
               n-available-bits (+ carry-bits 8)] ; Total available bits
-          ;;          (clojure.pprint/cl-format true "~%~D: ~8,'0b(~b) => ~b" carry-bits byte carry combined)
+          (clojure.pprint/cl-format true "~%~D: ~8,'0b(~b) => ~b" carry-bits byte carry combined)
           (if (>= n-available-bits bit-length)
             ;; If we have enough bits to extract an 11-bit value
             (let [shift-amount (- n-available-bits bit-length) ; Calculate how many bits to shift to extract the 11 bits
                   extracted-bits (bit-and (bit-shift-right combined shift-amount) 0x7FF) ; Extract the 11 bit
                   leftover-value (remove-first-n-bits bit-length combined n-available-bits)] ; Get remaining bits
-              ;;              (clojure.pprint/cl-format true " -> ~b" leftover-value)
+              (clojure.pprint/cl-format true " -> ~b" leftover-value)
               (recur more-bytes
                      leftover-value
                      shift-amount
@@ -135,6 +200,7 @@
                    (dec count))))))))
 
 (defn decode-compact-qr [seed-bytes len lang-key]
+  (assert (zero? (mod len 4)))
   (map #(get-in +word-list+ [lang-key %]) (extract-11-bits-dynamic seed-bytes len)))
 
 ;; 4: Byte mode indicator
@@ -163,11 +229,6 @@
                      (bit-shift-right (bit-and byte1 0xf0) 4))]
      (case mode
        :numeric (decode-standard-qr str lang-key)
-       :byte (-> byte1
-                 (bit-and 0x0f)
-                 (cons (nthrest raw 2))
-                 (decode-compact-qr
-                  len
-                  lang-key))))))
+       :byte (decode-compact-qr raw len lang-key)))))
 
 ;;(decode-seed-qr "175603411269118717760390053917171617011703680513069916681107050611591746089620340812129017111526")
