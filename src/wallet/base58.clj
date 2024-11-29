50643c594755f788875964d39dbb50806c8680f3
jc <oz.jongwon.choi@gmail.com>
2024-11-29 11:55:07 +1100

    add double-sha256, encode-check, and decode-check (important) Use bit-and instead of byte

--- src/wallet/base58.clj
+++ src/wallet/base58.clj
@@ -1,4 +1,14 @@
-(ns wallet.base58)
+;;;
+;;; Borrowed from https://github.com/rm-hull/base58
+;;;
+
+(ns wallet.base58
+  (:require [buddy.core.hash :as hash]))
+
+(defn double-sha256 [bytes]
+  (-> bytes
+      hash/sha256
+      hash/sha256))
 
 (defonce alphabet (vec "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"))
 
@@ -41,8 +51,21 @@
        (apply str)))
 
 (defn encode [value]
-  (pipeline 256 58 byte alphabet zero? (first alphabet) value))
+  (pipeline 256 58 #(bit-and % 0xff) alphabet zero? (first alphabet) value))
 
 (defn decode [value]
   (->> (drop-while first-char? value)
        (pipeline 58 256 inverted-alphabet char first-char? "\000")))
+
+(defn encode-check [v]
+  (encode `[~@v ~@(->> v (double-sha256) (take 4) ;(map #(bit-and % 0xff))
+                       )]))
+
+(defn decode-check [s]
+  (let [b (decode s)
+        checksum (take-last 4 b)
+        msg (drop-last 4 b)]
+    (if (= checksum
+           (->> msg byte-array double-sha256 (take 4)))
+      (byte-array msg)
+      (throw (ex-info "Checksum failed" {:expect checksum})))))
