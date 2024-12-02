4993224f8a7bf80187243c9318eef806ff6b9524
jc <oz.jongwon.choi@gmail.com>
2024-12-02 17:15:44 +1100

    minor cleanup

--- src/wallet/bip32.clj
+++ src/wallet/bip32.clj
@@ -44,44 +44,25 @@
    (let [raw (mac/hash seed {:key "Bitcoin seed" :alg :hmac+sha512})
          ;; Split into private key and chain code
          private-bytes (byte-array (take 32 raw))
-         chain-code (byte-array (drop 32 raw))
-         ;; Create private key from first 32 bytes
-         private-key (create-private-key private-bytes)]
-     ;; 4. Generating the Master Public Key
-     ;; Using the secp256k1 elliptic curve, the master private key is used to compute the corresponding master public key.
-     ;; This involves a point multiplication operation on the secp256k1 curve, resulting in a public key.
+         chain-code (byte-array (drop 32 raw))]
+     (validate-private-key private-bytes)
+     (->HDPrivateKey private-bytes chain-code version 0 0 0))))
 
-     ;; 5. Output
-     ;; The output is a structure containing:
-     ;; The master private key.
-     ;; The corresponding master public key.
-     ;; The chain code.
-     ;; Together, these components form the root of the HD wallet, enabling the derivation of child keys (both private and public) in a deterministic way.
-
-     (->HDPrivateKey private-key chain-code version))))
-
-(defn- int->n-byte-array [i n]
-  (let [bytes (byte-array n)]
-    (run! #(aset-byte bytes % (bit-and (bit-shift-right i (* 8 (- n %))) 0xff)) (range 4))
-    bytes))
-
-(defn private-key->xprv [{:keys [private-key chain-code version]}]
-  (let [depth 0
-        parent-fingerprint 0
-        child-index 0
-        raw-key-bytes (let [len (count private-key)]
+(defn private-key->xprv [{:keys [key chain-code version depth fingerprint]}]
+  (let [child-index 0
+        raw-key-bytes (let [len (count key)]
                         (case len
-                          33 private-key
+                          33 key
 
                           (31 32)
                           (let [padded-bytes (byte-array 33)
                                 n-bytes (- 33 len)]
                             (dotimes [i n-bytes]
                               (aset-byte padded-bytes i 0x00))
-                            (->> private-key
+                            (->> key
                                  count
                                  (min 32)
-                                 (System/arraycopy private-key 0 padded-bytes n-bytes))
+                                 (System/arraycopy key 0 padded-bytes n-bytes))
                             padded-bytes)))]
     ;; Check with
     ;;https://learnmeabitcoin.com/technical/keys/hd-wallets/extended-keys/
@@ -98,7 +79,7 @@
     ;;               (apply str)))
     (b58/encode-check (byte-array `[~@(vec version) ; 4 bytes
                                     ~depth          ; 1 byte
-                                    ~@(vec (int->n-byte-array parent-fingerprint 4)) ; 4 bytes
+                                    ~@(vec (int->n-byte-array fingerprint 4)) ; 4 bytes
                                     ~@(vec (int->n-byte-array child-index 4)) ; 4 bytes
                                     ~@(vec chain-code)                        ; 32 bytes
                                     ~@(vec raw-key-bytes)]))))
