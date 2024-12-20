d7e7fee8180f1321f2ad0f1947ed1876b228b54e
jc <oz.jongwon.choi@gmail.com>
2024-12-20 18:02:24 +1100

    fix padding by adding padding condition

--- src/wallet/segwit_addr.clj
+++ src/wallet/segwit_addr.clj
@@ -30,33 +30,36 @@
     [(Integer/toString (:data bit2) 2)
      (Integer/toString (:data bit3) 2)]))
 
-(defn n-bits->m-bits [data from-bits to-bits]
-  (letfn [(process-carry [carry]
-            (loop [{:keys [data size] :as bit-data} carry result []]
-              (cond (zero? size) [result nil]
-                    (= size to-bits) [(conj result data) nil]
-                    (< size to-bits) [result (make-bit-data data size)]
-                    :else (let [[a b] (bit-split bit-data to-bits)]
-                            (recur b (conj result (:data a)))))))]
-    (loop [[value & more-values] data
-           carry (make-bit-data 0 0)
-           result []]
-      (if value
-        (let [[data-to-add next-carry] (->> from-bits
-                                            (make-bit-data value)
-                                            (bit-merge carry)
-                                            process-carry)]
-          (recur more-values next-carry (into result data-to-add)))
-        (let [[carry-data {:keys [data size]}] (process-carry carry)]
-          (into result (conj carry-data (if (zero? data)
-                                          data ;; padding
-                                          (bit-shift-left data (- to-bits size))))))))))
+(defn n-bits->m-bits
+  ([data from-bits to-bits]
+   (n-bits->m-bits data from-bits to-bits true))
+  ([data from-bits to-bits padding?]
+   (letfn [(process-carry [carry]
+             (loop [{:keys [data size] :as bit-data} carry result []]
+               (cond (zero? size) [result nil]
+                     (= size to-bits) [(conj result data) nil]
+                     (< size to-bits) [result (make-bit-data data size)]
+                     :else (let [[a b] (bit-split bit-data to-bits)]
+                             (recur b (conj result (:data a)))))))]
+     (loop [[value & more-values] data
+            carry (make-bit-data 0 0)
+            result []]
+       (if value
+         (let [[data-to-add next-carry] (->> from-bits
+                                             (make-bit-data value)
+                                             (bit-merge carry)
+                                             process-carry)]
+           (recur more-values next-carry (into result data-to-add)))
+         (let [[carry-data {:keys [data size]}] (process-carry carry)]
+           (into result (cond (and padding? (zero? data)) (conj carry-data data)
+                              (zero? data) carry-data
+                              :else (conj carry-data (bit-shift-left data (- to-bits size)))))))))))
 
 (comment
   (for [n [2r10101100 2r0000000100 2r11111111 2r01010101 2r00001111
            2r11111000 2r11100000 2r00000001 2r00001111]]
     (let [eight-bitv (n-bits->m-bits [n] 8 5)]
-      ;;      (println "***" n eight-bitv (n-bits->m-bits eight-bitv 5 8))
-      (= [n] (n-bits->m-bits eight-bitv 5 8))))
+      ;; (println "***222" n eight-bitv (n-bits->m-bits eight-bitv 5 8 false))
+      (= [n] (n-bits->m-bits eight-bitv 5 8 false))))
 
   )
